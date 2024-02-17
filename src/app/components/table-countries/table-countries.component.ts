fb73d73640a73491d0b4ec7011f23112ad77637e
JuanCarlos-27 <jcromero909@misena.edu.co>
2024-02-17 13:37:22 -0500

    Add defer load of view row component

--- src/app/components/table-countries/table-countries.component.ts
+++ src/app/components/table-countries/table-countries.component.ts
@@ -1,11 +1,15 @@
 import { Country } from '@/interfaces/country.interface';
-import { DecimalPipe } from '@angular/common';
+import {
+  DecimalPipe,
+  NgComponentOutlet,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'countries-table',
   standalone: true,
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, NgTemplateOutlet],
   templateUrl: './table-countries.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
